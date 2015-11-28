7d87aa38e92c32dc54d247cd5f18a7cd2caed427
Bruno Garate <bruno.garate@gmail.com>
2015-11-27 21:50:48 -0300

    Trabajo sobre el algoritmo

--- usr/src/linux-4.1.2/kernel/tsoalloc/tsoalloc.c
+++ usr/src/linux-4.1.2/kernel/tsoalloc/tsoalloc.c
@@ -11,14 +11,19 @@
 
 static ALLOCATION_STRATEGY current_strategy = FIRST_FIT;
 
+enum FIT_CONDITIONS {
+  OK,
+  START,
+  NO_FIT
+}
+
 inline void* __region_end(struct tso_mm_region * region) {
   return (void*)(&region + sizeof(struct tso_mm_region) + region->size);
 }
 
-struct tso_mm_mapping * __tso_mm_initialize(void) {
+struct tso_mm_mapping * tso_mm_initialize(void) {
   struct tso_mm_mapping * mm = kmalloc(sizeof(struct tso_mm_mapping), GFP_KERNEL);
 
-  //mm->start = (void*) do_mmap(NULL, NULL, INITIAL_SIZE, PROT_READ | PROT_WRITE, 0, 0);
   mm->start = (void*) vm_mmap(NULL, NULL, INITIAL_SIZE, PROT_READ | PROT_WRITE, 0, 0);
   mm->size = INITIAL_SIZE;
   mm->free = INITIAL_SIZE;
@@ -31,73 +36,103 @@ void __tso_mm_finalize(void) {
 
 }
 
-inline struct tso_mm_region* __tso_first_region(void) {
-  return (struct tso_mm_region*)current_mm->start;
-}
-
 void __tso_mm_expand(void) {
   
 }
 
-struct tso_mm_region* __tso_mm_get_before_best_fit(size_t size) {
+inline size_t region_size(size_t size) {
+  return sizeof(struct tso_mm_region) + size;
 }
 
-struct tso_mm_region* __tso_mm_get_before_worst_fit(size_t size) {
-}
+struct tso_mm_region* tso_mm_get_fit(size_t size, enum FIT_CONDITIONS* state) {
 
-struct tso_mm_region* __tso_mm_get_before_first_fit(size_t size) {
-  struct tso_mm_region * current_region = __tso_first_region();
+  struct tso_mm_region* next_region = current_mm->first_region;
 
-  //if(current_region == NULL)
-  //  return NULL;
-
-  return NULL;
-}
-
-struct tso_mm_region* __tso_mm_get_before_fit(size_t size) {
-  switch(current_strategy) {
-  case WORST_FIT:
-    return __tso_mm_get_before_worst_fit(size);
-    break;
-  case BEST_FIT:
-    return __tso_mm_get_before_best_fit(size);
-    break;
-  case FIRST_FIT:
-    return __tso_mm_get_before_first_fit(size);
-    break;
+  if(next_region == NULL) {
+    state = START;
+    return NULL;
   }
+
+  struct tso_mm_region* previous_region = NULL;
+  struct tso_mm_region* best_match = NULL;
+
+  long best_size = 0;
+  long available_size = &next_region - current_mm->start;
+
+  while(next_region != NULL) {
+
+    if(available_size > region_size(size)) {
+
+      bool isBest = false;
+
+      switch(current_strategy) {
+        case WORST_FIT:
+          isBest = available_size < best_size;
+          break;
+        case BEST_FIT:
+          isBest = available_size > best_size;
+          break;
+        case FIRST_FIT:
+          state = OK;
+          return (tso_mm_region*)&previous_region;
+        }
+
+        if (isBest) {
+          best_size = available_size;
+          best_match = previous_region;
+        }
+
+    }
+
+    previous_region = next_region;
+    next_region = next_region->next;
+    available_size = &next_region - &previous_region - sizeof(struct tso_mm_region);
+  }
+
+  state = best_match != NULL ? OK : NO_FIT;
+  return best_match;
+
 }
 
-void __tso_mm_remove_region(struct tso_mm_region * region) {
-
-}
-
-void __tso_mm_split_region(struct tso_mm_region * previous, struct tso_mm_region * freed_region, struct tso_mm_region * next) {
-
-}
-
-void __tso_mm_join_region(struct tso_mm_region * previous, struct tso_mm_region * freed_region, struct tso_mm_region * next) {
-
-}
-
-asmlinkage long sys_tso_mm_alloc(size_t size, void* ret) {
+asmlinkage long sys_tso_mm_alloc(size_t size, void* address) {
   struct tso_mm_region* new_region;
+
+  address = NULL;
+
+  if (current_mm == NULL)
+    tso_mm_initialize();
 
   if(current_mm->free < size)
     return -1;
 
-  struct tso_mm_region* fit = __tso_mm_get_before_fit(size);
+  enum FIT_CONDITIONS state;
+  struct tso_mm_region* fit = tso_mm_get_before_fit(size, &size);
 
-  if(fit == NULL)
-    return -1;
+  long position;
 
-  new_region = (struct tso_mm_region*)(__region_end(fit) + 1);
+  switch(state) {
+    case OK:
+      position = &fit + sizeof(tso_mm_region);
+    case START:
+      position = current_mm->start;
+      break;
+    default:
+      return -1;
+  }
+  
+  new_region = (struct tso_mm_region*)(position);
   new_region->size = size;
 
   new_region->next = fit->next;
-  fit->next = new_region;
+  
+  if(state == OK)
+    fit->next = new_region;
+  else if(state == START)
+    current_mm->first_region = fit;
 
-  //ret = __to_user_address(new_region);
+
+  void* res = (void*)(&new_region + sizeof(tso_mm_region))
+  copy_to_user(&address, res, sizeof(void*));
 
   return 0;
 }
